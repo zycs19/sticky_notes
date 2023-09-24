3766492d1c4cf1ce60fe418c1c87a338841caf2d
ying <zycs19@gmail.com>
2023-09-24 12:27:49 -0700

    minor edits

--- models/Note.js
+++ models/Note.js
@@ -1,35 +1,35 @@
 const mongoose = require('mongoose')
 const AutoIncrement = require('mongoose-sequence')(mongoose)
 
-const noteScheme = new mongoose.Schema(
+const noteSchema = new mongoose.Schema(
     {
-    user: {
-        type: mongoose.Schema.Types.ObjectId, 
-        required: true,
-        ref:'User'
-    },
-    title: {
-        type: String, 
-        required: true
-    },
-    text: {
-        type: String, 
-        required: true
-    },
-    completed: {
-        type: Boolean, 
-        default: false
-    }
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
     },
     {
         timestamps: true
     }
 )
 
-noteScheme.plugin(AutoIncrement, {
+noteSchema.plugin(AutoIncrement, {
     inc_field: 'ticket',
     id: 'ticketNums',
     start_seq: 500
 })
 
-module.exports = mongoose.model('Note', noteScheme)
+module.exports = mongoose.model('Note', noteSchema)
